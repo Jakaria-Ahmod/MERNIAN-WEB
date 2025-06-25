4b349d0465149d89774a6ce0a28851d8987b944f
md jakria ahmod <jakariaahmodmd@gmail.com>
2025-06-25 12:46:18 +0600

    file path not work

--- src/pages/Friends/index.jsx
+++ src/pages/Friends/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ProfileBanner from '../../globalComponents/profileBanner';
-import AllFriends from './components/allFriends';
+import AllFriends from './components/AllFriends';
 
 const Fridends = () => {
   return (
